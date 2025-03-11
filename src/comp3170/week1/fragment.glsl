a3d12c7b644edd56bed76b4c2f368d259590c669
RL <rhiannon.luo1@gmail.com>
2025-03-12 09:15:34 +1100

    Modified code to change shape
    Changed the code to modify shape from circle to diamond.

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -1,18 +1,21 @@
 #version 410
 
-uniform vec3 u_colour;			// colour as a 3D vector (r,g,b)
-uniform vec2 u_screenSize;  	// screen dimensions in pixels
+uniform vec3 u_colour;    		// colour as a 3D vector (r,g,b)
+uniform vec2 u_screenSize;    	// screen dimensions in pixels
 
-layout(location = 0) out vec4 o_colour;	// output to colour buffer
+layout(location = 0) out vec4 o_colour;    // output to colour buffer
 
 void main() {
-   vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
-  
-   if (d < 0.5) {
-      o_colour = vec4(u_colour, 1);
-   }
-   else {
-      o_colour = vec4(0,0,0,1); // BLACK
-   }
+    vec2 p = gl_FragCoord.xy / u_screenSize; 	// scale p into range (0,0) to (1,1)
+
+    vec2 centered = p - vec2(0.5, 0.5); 		//center the shape
+
+    float d = abs(centered.x) + abs(centered.y); //calculate distance
+
+    if (d < 0.5) {					    // Set the threshold for the diamond shape 
+        o_colour = vec4(u_colour, 1);  //Diamond Colour
+    }
+    else {
+        o_colour = vec4(0, 0, 0, 1);  //Black
+    }
 }
